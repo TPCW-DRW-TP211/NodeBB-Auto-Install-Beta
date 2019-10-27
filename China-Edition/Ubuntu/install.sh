49b77f2b98e55148f7caf6d411f31077c5c7c5a3
TP211 <1242410592@qq.com>
2019-10-27 23:07:37 +0800

    修复source命令Bug

--- China-Edition/Ubuntu/install.sh
+++ China-Edition/Ubuntu/install.sh
@@ -19,15 +19,18 @@ apt-get update
 
 echo "[+] 安装NVM并安装/配置nodejs"
 curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.8/install.sh | bash
-source ~/.bashrc
+sleep 5
+source ~/.bashrc && echo "1"
 export NVM_NODEJS_ORG_MIRROR=https://npm.taobao.org/mirrors/node
 export NVM_IOJS_ORG_MIRROR=https://npm.taobao.org/mirrors/iojs
-source ~/.bashrc
+sleep 5
+source ~/.bashrc && echo "1"
 nvm install --lts
 node -v
 npm -v
 npm config set registry https://registry.npm.taobao.org
-source ~/.bashrc
+sleep 5
+source ~/.bashrc && echo "1"
 systemctl enable nodejs
 systemctl enable npm
 
