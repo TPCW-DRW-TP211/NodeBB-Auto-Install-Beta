851dddf0d12b207ff52301879535ae6969b18c0a
TP211 <1242410592@qq.com>
2020-09-28 06:27:43 +0800

    Update General_Install.sh
    加入操作系统检测(不是检测发行版)和运行权限检测

--- China-Edition/General_Install.sh
+++ China-Edition/General_Install.sh
@@ -1,1 +1,9 @@
 #!/bin/bash
+if["$(uname)"=="Darwin"];then
+    echo "暂不支持MacOS安装"
+elif["$(expr substr $(uname -s) 1 5)"=="Linux"];then   
+    if [ $(id -u) != "0" ]; then
+        echo "[#] 错误: 你必须使用root权限运行本脚本！"
+        exit 1
+    fi
+fi
